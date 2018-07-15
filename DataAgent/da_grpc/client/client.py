21c995b7f3570ca6195114a0a9c86ecf3329b491
B, Vinod K <vinod.k.b@intel.com>
2018-07-16 04:49:36 +0530

    VideoIngestion, DataAnalytics: gRPC issue fix for statuscode unavailable
    The issue was related to http proxy settings as grpc is based on http2.
    So before making the gRPC call, the http proxy settings are unset and
    restored after the client receives the gRPC call response.
    
    For time being, the deadline exceeded grpc issue is been addressed with
    1000 sec timeout which is a temporary fix.
    
    Change-Id: I35b89fb8503f08842bf99243a2aa9114ade05651
    Signed-off-by: B, Vinod K <vinod.k.b@intel.com>

--- DataAgent/da_grpc/client/client.py
+++ DataAgent/da_grpc/client/client.py
@@ -5,7 +5,7 @@ import grpc
 import json
 import DataAgent.da_grpc.protobuff.da_pb2 as da_pb2
 import DataAgent.da_grpc.protobuff.da_pb2_grpc as da_pb2_grpc
-
+import os
 
 class GrpcClient(object):
 
@@ -17,10 +17,28 @@ class GrpcClient(object):
             RedisCfg etc.,) returning the dictionary object for that
             particular config
         """
+        # Unsetting proxy to address StatusCode.Unavailable/putty sessions gRPC issue
+        http_proxy = None
+        https_proxy = None
+        if 'http_proxy' in os.environ:
+            http_proxy = os.environ['http_proxy']
+            del os.environ['http_proxy']
+
+        if 'https_proxy' in os.environ:
+            https_proxy = os.environ['https_proxy']
+            del os.environ['https_proxy']
+
         channel = grpc.insecure_channel('localhost:50051')
         stub = da_pb2_grpc.daStub(channel)
 
         response = stub.GetConfigInt(da_pb2.ConfigIntReq(cfgType=config),
-                                     timeout=1)
-        # print("response:" +  response.jsonMsg)
+                                     timeout=1000)
+
+        # Resetting proxy variables
+        if http_proxy is not None:
+            os.environ['http_proxy'] = http_proxy
+
+        if https_proxy is not None:
+            os.environ['https_proxy'] = https_proxy
+
         return json.loads(response.jsonMsg)
