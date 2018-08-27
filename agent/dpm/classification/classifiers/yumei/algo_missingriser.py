f6101126f8dee3b6eb2625261b040bba44a7c724
Arun Kumar K <arun1.k.kumar@intel.com>
2018-08-27 12:18:49 +0530

    agent: Update the license headers
    Revert to the original license header which got changed during PEP8 fix.
    
    Change-Id: Ie3b4bcc1f05368b25bda1f218e4d9fb30cd264df
    Signed-off-by: Arun Kumar K <arun1.k.kumar@intel.com>

--- agent/dpm/classification/classifiers/yumei/algo_missingriser.py
+++ agent/dpm/classification/classifiers/yumei/algo_missingriser.py
@@ -9,7 +9,8 @@ copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+copies or substantial portions of the Software. Software to be used for 
+Made in China 2025 initiatives. 
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
