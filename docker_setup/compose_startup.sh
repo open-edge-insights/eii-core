b95570f86ae4ead207215315d99411bee7eb9dbc
Vishwas r <vishwas.r@intel.com>
2019-01-16 15:42:03 +0530

    docker_setup: Added fix for bug iapoc9-339 (#221)
    Changes:
    1. Added a fix for docker compose logs not
       showing first few lines of the logs.
    
    Signed-off-by: Vishwas R <vishwas.r@intel.com>

--- docker_setup/compose_startup.sh
+++ docker_setup/compose_startup.sh
@@ -118,9 +118,7 @@ up_iei() {
 		docker-compose $OVERRIDE_COMPOSE_YML up &> $etaLogDir/consolidatedLogs/eta.log
 	else
 		echo "3. Creating and starting the dependency/eta containers..."
-		docker-compose $OVERRIDE_COMPOSE_YML up -d
-		#Logging the docker compose logs to file.
-		docker-compose logs -f &> $etaLogDir/consolidatedLogs/eta.log &
+		docker-compose $OVERRIDE_COMPOSE_YML up &> $etaLogDir/consolidatedLogs/eta.log &
 	fi
 }
 
