d5455655be9aab775e9c209a5b85f31293ea8ed4
S, Sandesh Kumar S <sandesh.kumar.s.s@intel.com>
2019-01-21 14:01:07 +0530

    docker_setup: Enable log level for data analytics and removal of entrypoint_classifier.sh (#238)
    The command line arguments for config file and log level has been
    added to the classifier start script.
    
    The entrypoint classifier script which is no longer used has been
    removed.
    
    Signed-off-by: sandeshk <sandesh.kumar.s.s@intel.com>

--- DataAnalytics/classifier_start.sh
+++ DataAnalytics/classifier_start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+configFile=$1
+logLevel=$2
 source /opt/intel/computer_vision_sdk_2018.4.420/bin/setupvars.sh
-python3.6 classifier_startup.py --log-dir /ETA/classifier_logs
+
+python3.6 classifier_startup.py --log-dir /ETA/classifier_logs --config $configFile --log $logLevel
--- DataAnalytics/entrypoint_classifier.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "1. Starting classifier.py, Running Kapacitor and Enabling the Classifier Task..."
-python3.6 classifier_startup.py --config `echo $1` --log-dir /ETA/classifier_logs
