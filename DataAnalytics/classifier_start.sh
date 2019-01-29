8fea51bfb9ec79fa0568311bd6f899431cab0eb4
npurusho <neha.purushothaman@intel.com>
2019-01-29 16:54:21 -0700

    ETA : Update to OpenVINO R5
    Signed-off-by: npurusho <neha.purushothaman@intel.com>

--- DataAnalytics/classifier_start.sh
+++ DataAnalytics/classifier_start.sh
@@ -2,6 +2,6 @@
 
 configFile=$1
 logLevel=$2
-source /opt/intel/computer_vision_sdk_2018.4.420/bin/setupvars.sh
+source /opt/intel/computer_vision_sdk/bin/setupvars.sh
 
 python3.6 classifier_startup.py --log-dir /ETA/classifier_logs --config $configFile --log $logLevel
