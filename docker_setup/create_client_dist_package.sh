7f8c5c81ece835dc1205845a8431cb061238f9d0
Kevin Midkiff <kevin.midkiff@intel.com>
2019-03-07 10:26:15 -0800

    Add auto-removal of container building dist_libs after run

--- docker_setup/create_client_dist_package.sh
+++ docker_setup/create_client_dist_package.sh
@@ -5,6 +5,6 @@ echo "Building dist_libs container..."
 docker build -f ../dist_libs/Dockerfile -t ia/ia_dist_libs ../
 
 echo "Running ia/ia_dist_libs image to create dist_libs..."
-docker run -v /opt/intel/iei/dist_libs:/iei/dist_libs ia/ia_dist_libs
+docker run --rm -v /opt/intel/iei/dist_libs:/iei/dist_libs ia/ia_dist_libs
 
 
