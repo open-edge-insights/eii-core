b4010e9ac83900bccee0fbf0717f0ba91318fea1
Vinod K B <vinod.k.b@intel.com>
2019-04-10 10:53:17 +0530

    dist_libs: Moved the base image version to 18.04
    To support native run of visualizer app on 18.04, i've
    updated the base image version of ubuntu to 18.04 in
    dist libs Dockerfile and made necessary changes
    
    Signed-off-by: Vinod K B <vinod.k.b@intel.com>

--- docker_setup/create_client_dist_package.sh
+++ docker_setup/create_client_dist_package.sh
@@ -2,9 +2,14 @@ echo "copying dockerignore file.."
 cp dockerignores/.dockerignore.dist_libs ../.dockerignore
 
 echo "Building dist_libs container..."
-docker build -f ../dist_libs/Dockerfile -t ia/ia_dist_libs ../
+docker build --build-arg UBUNTU_IMAGE_VERSION=18.04 \
+             -f ../dist_libs/Dockerfile \
+             -t ia/ia_dist_libs \
+             ../
 
 echo "Running ia/ia_dist_libs image to create dist_libs..."
-docker run --rm -v /opt/intel/iei/dist_libs:/iei/dist_libs ia/ia_dist_libs
+docker run --rm \
+            -v /opt/intel/iei/dist_libs:/iei/dist_libs \
+            ia/ia_dist_libs
 
 
