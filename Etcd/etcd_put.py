82a20a51da819ac5f8be67f2c6ab65f15ddff94c
Vinod K B <vinod.k.b@intel.com>
2019-07-23 20:32:03 +0530

    Etcd: Added the helper scripts to work with etcd
    Added in the etcd_pre_load.json, scripts to start etcd
    container and load the mentioned json to load VideoIngestion
    and VideoAnalytics services
    
    Signed-off-by: Vinod K B <vinod.k.b@intel.com>

---
+++ Etcd/etcd_put.py
@@ -0,0 +1,21 @@
+import etcd3
+import json
+
+def main():
+    etcd = etcd3.client()
+    with open("etcd_pre_load.json", 'r') as f:
+        config = json.load(f)
+    
+    print("=======Adding key/values to etcd========")
+    for key, value in config.items():
+        if isinstance(value, str):
+            etcd.put(key, bytes(value.encode()))
+        elif isinstance(value, dict):
+            etcd.put(key, bytes(json.dumps(value).encode()))
+
+    print("=======Reading key/values to etcd========")
+    for key in config.keys():
+        value = etcd.get(key)
+        print(key, '->', value[0].decode())
+        
+main()    
