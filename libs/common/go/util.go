db04806257baef0aa346ffff403453992416e9ac
vka <varalakshmi.ka@intel.com>
2019-08-07 15:52:54 +0530

    libs/common, OpcuaExport: Added security configurations in Util.go
    1. Added utility function which construts a conig to align with security,
       based on devmode, topic type pub/sub
    2. Enabled message bus security in OpcuaExport
    
    Signed-off-by: vka <varalakshmi.ka@intel.com>

--- libs/common/go/util.go
+++ libs/common/go/util.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	configmgr "IEdgeInsights/libs/ConfigManager"
+	"log"
 	"net"
 	"os"
 	"strconv"
@@ -32,46 +34,93 @@ func CheckPortAvailability(hostname, port string) bool {
 	return portUp
 }
 
-//GetTopicConfig - returns topic related congigurations
-func GetTopicConfig(topic string, topicType string) map[string]interface{} {
-	subConfigList := strings.Split(os.Getenv(topic+"_cfg"), ",")
-	var config map[string]interface{}
+// GetMessageBusConfig - constrcuts config object based on topic type(pub/sub),
+// message bus type(tcp/ipc) and dev/prod mode
+func GetMessageBusConfig(topic string, topicType string, devMode bool, cfgMgrConfig map[string]string) map[string]interface{}{
+	var subTopics []string
+	appName := os.Getenv("AppName")
+	cfgMgrCli := configmgr.Init("etcd", cfgMgrConfig)
+	if strings.ToLower(topicType) == "sub"{
+		subTopics = strings.Split(topic, "/")
+                topic = subTopics[1]
+	}
+	topicConfigList := strings.Split(os.Getenv(topic+"_cfg"), ",")
+	var messageBusConfig map[string]interface{}
 
-	if subConfigList[0] == "zmq_tcp" {
-		address := strings.Split(subConfigList[1], ":")
+	if topicConfigList[0] == "zmq_tcp" {
+		address := strings.Split(topicConfigList[1], ":")
 		hostname := address[0]
 		port, err := strconv.ParseInt(address[1], 10, 64)
 		if err != nil {
 			glog.Errorf("string to int64 converstion Error: %v", err)
 			os.Exit(1)
 		}
-		host := map[string]interface{}{
+		hostConfig := map[string]interface{}{
 			"host": hostname,
 			"port": port,
 		}
+
 		if strings.ToLower(topicType) == "pub" {
-			config = map[string]interface{}{
+			messageBusConfig = map[string]interface{}{
 				"type":            "zmq_tcp",
-				"zmq_tcp_publish": host,
+				"zmq_tcp_publish": hostConfig,
+			}
+
+			if !devMode {
+				var allowedClients []string
+				subscribers := strings.Split(os.Getenv("Subscribers"), ",")
+				for _, subscriber := range subscribers {
+					clientPublicKey, err := cfgMgrCli.GetConfig("/Publickeys/" + subscriber)
+					if err != nil {
+						glog.Errorf("Etcd GetConfig Error %v", err)
+						os.Exit(1)
+					}
+					allowedClients = append(allowedClients, clientPublicKey)
+				}
+				serverSecretKey, err := cfgMgrCli.GetConfig("/" + appName + "/private_key")
+				if err != nil {
+					log.Fatal(err)
+				}
+				messageBusConfig["allowed_clients"] = allowedClients
+				hostConfig["server_secret_key"] = serverSecretKey
 			}
 		} else if strings.ToLower(topicType) == "sub" {
-			config = map[string]interface{}{
-				"type": "zmq_tcp",
-				topic:  host,
+			messageBusConfig = map[string]interface{}{
+				"type":       "zmq_tcp",
+				topic: hostConfig,
+			}
+			if !devMode {
+				serverPublicKey, err := cfgMgrCli.GetConfig("/Publickeys/" + subTopics[0])
+				if err != nil {
+					glog.Errorf("Etcd GetConfig Error %v", err)
+					os.Exit(1)
+				}
+				clientSecretKey, err := cfgMgrCli.GetConfig("/" + appName + "/private_key")
+				if err != nil {
+					glog.Errorf("Etcd GetConfig Error %v", err)
+					os.Exit(1)
+				}
+				clientPublicKey, err := cfgMgrCli.GetConfig("/Publickeys/" + appName)
+				if err != nil {
+					glog.Errorf("Etcd GetConfig Error %v", err)
+					os.Exit(1)
+				}
+				hostConfig["server_public_key"] = serverPublicKey
+				hostConfig["client_secret_key"] = clientSecretKey
+				hostConfig["client_public_key"] = clientPublicKey
 			}
 		} else {
 			panic("Unsupported Topic Type!!!")
 		}
-
-	} else if subConfigList[0] == "zmq_ipc" {
-		config = map[string]interface{}{
+	} else if topicConfigList[0] == "zmq_ipc" {
+		messageBusConfig = map[string]interface{}{
 			"type":       "zmq_ipc",
-			"socket_dir": subConfigList[1],
+			"socket_dir": topicConfigList[1],
 		}
 	} else {
 		panic("Unsupported MessageBus Type!!!")
 	}
-	return config
+	return messageBusConfig
 }
 
 //GetTopics - returns list of topics based on topic type
@@ -119,6 +168,7 @@ func GetEndPointMap(keywordName string, endPointInfo string) (map[string]interfa
 		portNumber, err := strconv.ParseInt(strings.TrimSpace(parts[1]), 10, 64)
 		if err != nil {
 			glog.Errorf("Not able to convert the port to Int %s", err)
+			os.Exit(1)
 		}
 		endPointInfo["port"] = portNumber
 		infoEndPoint[keywordName] = endPointInfo
