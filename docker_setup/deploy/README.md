96c12184a361e2555d50cbbdd82a770a58882bbf
Vivek R Soundararajan <vivek.rs@intel.com>
2019-10-12 01:26:21 +0530

    deploy: EIS Bundle Generator
    This Utility helps to Generate the bundle to deploy EIS.
    
    Signed-off-by: Vivek R Soundararajan <vivek.rs@intel.com>

--- docker_setup/deploy/README.md
+++ docker_setup/deploy/README.md
@@ -22,21 +22,47 @@ EIS Deployment will be done only TurtleCreek Installed & Provisioned Device via 
             "docker_compose_file_version": "<docker_compose_file_version which is compose file version supported by deploying docker engine>",
             
             "exclude_services": [list of services which you want exclude for your deployement]
+            "include_services": [list of services needed in final deployment docker-compose.yml]
 
         }        
       ```
     ***Note***: Please validate the json.
     Also Please ensure that you have updated the DOCKER_REGISTRY in [docker_setup/.env](../.env) file
 
-3. Follow the below commands for Telit bundle Generation.
+3. Follow the below commands for EIS bundle Generation.
     ```
-    sudo python3.6 generate_tc_bundle.py
+    sudo python3.6 generate_eis_bundle.py
     ```
-4. Telit Bundle **eis_installer.tar.gz** will be generated under the same directory.
+4. EIS Bundle **eis_installer.tar.gz** will be generated under the same directory.
 
 5. Follow the TurtleCreek Repo on How to deploy this bundle with Telit Portal
 
 **Note** :
+
+    Default Values of Bundle Name is "eis_installer.tar.gz" and the tag name is "eis_installer"
+
+    Using *-t* options you can give your custom tag name which will be used as same name for bundle generation.
+
+    Default values for docker-compose yml path is "../docker-compose.yml"
+
+    using "-f" you can give any docker-compose yml file path. But make sure that you have provisioned that already.
+
+
+For more help:
+        ```
+            sudo python3.6 generate_eis_bundle.py
+        ```
+
+
+    usage: generate_eis_bundle.py [-h] [-f COMPOSE_FILE_PATH] [-t BUNDLE_TAG_NAME]
+
+    EIS Bundle Generator: This Utility helps to Generate the bundle to deploy EIS.
+
+    optional arguments:
+        -h, --help            show this help message and exit
+        -f COMPOSE_FILE_PATH  Docker Compose File path for EIS Deployment (default: ../docker-compose.yml)
+        -t BUNDLE_TAG_NAME    Tag Name used for Bundle Generation (default:eis_installer)
+
     While deploying EIS Software Stack via Telit-TurtleCreek. Visualizer UI will not pop up because of display not attached to docker container of Visualizer.
 
     To Overcome. goto IEdgeInsights/docker_setup 
---
+++ docker_setup/deploy/generate_eis_bundle.py
@@ -0,0 +1,150 @@
+import yaml
+import subprocess
+import json
+import os
+import argparse
+
+DEFAULT_CERTS = ["ca", "root"]
+
+
+class EisBundleGenerator:
+
+    def __init__(self):
+        '''
+            Intial Configuration will be initialised here.
+        '''
+        with open('config.json', 'r') as utilconfig:
+            config = json.load(utilconfig)
+            self.docker_compose_file_version = \
+                config['docker_compose_file_version']
+            self.exclude_services = config['exclude_services']
+            self.include_services = config['include_services']
+            self.env = self.get_env_dict()
+            if self.env['DOCKER_REGISTRY'] is '' or \
+                    "/" not in self.env['DOCKER_REGISTRY']:
+                print("Please Check the Docker Regsitry Address in \
+                'DOCKER_REGISTRY' env of docker_setup\\.env file")
+                os._exit(1)
+
+    def get_env_dict(self):
+        '''
+            This method reads the .env file and returns as
+            dict values
+        '''
+        with open("../.env") as f:
+            envList = f.readlines()
+
+        envDict = {}
+        for env in envList:
+            if env.strip() and '#' not in env.strip()[0]:
+                envVal = env.split('=')
+                envDict[envVal[0]] = envVal[1].rstrip()
+        return envDict
+
+    def generate_docker_composeyml(self):
+        '''
+            This method helps to generate the docker-compose yaml
+            file from EIS "docker-setup" directory and manipualtes
+            new compose yaml file as per config file which will be
+            part of telit bundle
+        '''
+        try:
+            with open(self.docker_file_path, 'r') as ymlfile:
+                self.config = yaml.load(ymlfile)
+            self.config['version'] = self.docker_compose_file_version
+
+            for service in self.config['services'].keys():
+                if service not in self.include_services:
+                    self.exclude_services.append(service)
+
+            # Remove Unwanted Services for Build
+            for service in self.exclude_services:
+                if service in self.config['services']:
+                    del self.config['services'][service]
+
+            # Remove Build Option & depends_on Option from docker-compose
+            for service in self.config['services'].keys():
+                if 'build' in self.config['services'][service].keys():
+                    del self.config['services'][service]['build']
+                if 'depends_on' in self.config['services'][service].keys():
+                    del self.config['services'][service]['depends_on']
+
+            # Write the Docker Compose File to System
+            with open("docker-compose.yml", 'w') as tcconf:
+                yaml.dump(self.config, tcconf, default_flow_style=False)
+        except Exception as e:
+            print("Exception Occured", e)
+
+    def generate_eis_bundle(self):
+        '''
+            generateTelitBundle helps to execute set of pre
+            commands which is required for TC Bundle and finally
+            it generates the bundle
+        '''
+
+        eis_cert_dir = "./" + self.bundle_tag_name + "/provision/Certificates/"
+        cmdlist = []
+        cmdlist.append("rm -rf " + self.bundle_tag_name)
+        cmdlist.append("mkdir -p " + self.bundle_tag_name)
+        cmdlist.append("mv docker-compose.yml ./" + self.bundle_tag_name)
+        cmdlist.append("cp -rf ../config ./" + self.bundle_tag_name)
+        cmdlist.append("cp ../.env ./" + self.bundle_tag_name)
+        cmdlist.append("cp -rf ../test_videos/ ./" + self.bundle_tag_name)
+        if self.env["DEV_MODE"] == "false":
+            cmdlist.append("mkdir -p " + self.bundle_tag_name + "/provision")
+            for service in self.config['services'].keys():
+                servicename =\
+                    self.config['services'][service]['environment']['AppName']
+                cmdlist.append("mkdir -p " + eis_cert_dir + servicename)
+                cmdlist.append("cp -rf " +
+                               "../provision/Certificates/" +
+                               servicename + "/ " + eis_cert_dir)
+            
+            for cert in DEFAULT_CERTS:
+                cmdlist.append("cp -rf " +
+                               "../provision/Certificates/" +
+                               cert + "/ " + eis_cert_dir)
+
+        cmdlist.append("chown -R eisuser:eisuser ./" + self.bundle_tag_name)
+        cmdlist.append("tar -czvf \
+                " + self.bundle_tag_name + ".tar.gz ./" + self.bundle_tag_name)
+        if self.bundle_folder is False:
+            cmdlist.append("rm -rf " + self.bundle_tag_name + "/")
+        else:
+            pass
+        try:
+            for cmd in cmdlist:
+                subprocess.check_output(cmd, shell=True)
+            print("TurtleCreek Bundle Generated Succesfully")
+        except Exception as e:
+            print("Exception Occured ", str(e))
+
+    def main(self, args):
+        self.bundle_tag_name = args.bundle_tag_name
+        self.docker_file_path = args.compose_file_path
+        self.bundle_folder = args.bundle_folder
+        self.generate_docker_composeyml()
+        self.generate_eis_bundle()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="EIS Bundle Generator:\
+         This Utility helps to Generate the bundle required to deploy EIS.\
+         ", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-f',
+                        dest='compose_file_path',
+                        default="../docker-compose.yml",
+                        help='Docker Compose File path for EIS Deployment')
+    parser.add_argument('-t',
+                        dest='bundle_tag_name',
+                        default="eis_bundle",
+                        help='Tag Name used for Bundle Generation')
+    parser.add_argument('-bf',
+                        dest='bundle_folder',
+                        default=False,
+                        help='This Flag is set not to generate \
+                        bundleName.tar.gz file.For Only Bundle folder')
+    args = parser.parse_args()
+
+    eisBundle = EisBundleGenerator()
+    eisBundle.main(args)
