6d9fab6b6adf12bf656ab12362a784a955102516
Vatika <vatika.gupta@intel.com>
2020-09-11 13:18:57 -0400

    provision/gen_certs.py: Fix secret apply command
    for namespace for latest k8s version
    
    Signed-off-by: Vatika <vatika.gupta@intel.com>

--- build/provision/gen_certs.py
+++ build/provision/gen_certs.py
@@ -146,8 +146,9 @@ def clean():
 def generate_k8s_secrets():
     try:
        subprocess.run (["kubectl" , "create" , "secret" , "generic" , "ca-etcd" , "--from-file=Certificates/ca/ca_certificate.pem" ])
-       cmd1 = subprocess.run (["kubectl" , "get" , "secret" , "ca-etcd" , "--namespace=default" , "--export" , "-o" ,"yaml" ],stdout=subprocess.PIPE, check=False)
-       subprocess.run(["kubectl", "apply", "--namespace=eis", "-f","-"] , input=cmd1.stdout, check=False)
+       cmd1 = subprocess.run (["kubectl" , "get" , "secret" , "ca-etcd" , "--namespace=default" , "-o" ,"yaml" ],stdout=subprocess.PIPE, check=False)
+       cmd2 = subprocess.run (["grep" , "-v" , "^\s*namespace:\s" ] , input=cmd1.stdout,stdout=subprocess.PIPE, check=False)
+       subprocess.run(["kubectl", "apply", "--namespace=eis", "-f","-"] , input=cmd2.stdout, check=False)
        for key,value in data.items():
            for var in value:
                for k,v in var.items():
@@ -155,10 +156,12 @@ def generate_k8s_secrets():
                    cs = list(v)[0].split("_")[0]
                    subprocess.run (["kubectl", "create", "secret", "generic" ,k1+"-cert","--from-file=Certificates/"+k+"/"+k+"_"+cs+"_certificate.pem"])
                    subprocess.run (["kubectl", "create", "secret", "generic" ,k1+"-key", "--from-file=Certificates/"+k+"/"+k+"_"+cs+"_key.pem"])
-                   cmd2 = subprocess.run (["kubectl", "get", "secret", k1+"-cert", "--namespace=default", "--export", "-o", "yaml"],stdout=subprocess.PIPE, check=False)
-                   subprocess.run ([ "kubectl", "apply", "--namespace=eis", "-f", "-"] , input=cmd2.stdout, check=False)
-                   cmd3 = subprocess.run (["kubectl", "get", "secret", k1+"-key", "--namespace=default", "--export", "-o", "yaml"],stdout=subprocess.PIPE, check=False)
-                   subprocess.run (["kubectl", "apply", "--namespace=eis", "-f", "-"],input=cmd3.stdout, check=False)
+                   cmd3 = subprocess.run (["kubectl", "get", "secret", k1+"-cert", "--namespace=default", "-o", "yaml"],stdout=subprocess.PIPE, check=False)
+                   cmd4 = subprocess.run (["grep" , "-v" , "^\s*namespace:\s" ],input=cmd3.stdout,stdout=subprocess.PIPE, check=False)
+                   subprocess.run ([ "kubectl", "apply", "--namespace=eis", "-f", "-"] , input=cmd4.stdout, check=False)
+                   cmd5 = subprocess.run (["kubectl", "get", "secret", k1+"-key", "--namespace=default", "-o", "yaml"],stdout=subprocess.PIPE, check=False)
+                   cmd6 = subprocess.run (["grep" , "-v" , "^\s*namespace:\s" ], input=cmd5.stdout,stdout=subprocess.PIPE, check=False)
+                   subprocess.run (["kubectl", "apply", "--namespace=eis", "-f", "-"],input=cmd6.stdout, check=False)
     except Exception as err:
           print("Exception Occured in generating k8s secrets" + str(err))
           sys.exit(1)
